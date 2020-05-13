db15c3361c6f930d24ababc6226c40728c1e1e7f
rorytboyle <rorytboyle@gmail.com>
2020-05-13 10:55:28 +0100

    Add files via upload
    Code for extracting Shen Atlas ROIs from preprocessed timeseries images using Windows Powershell

---
+++ extractShenROIs.ps1
@@ -0,0 +1,24 @@
+<# 
+This script contains Windows Powershell Code for computing Shen Atlas ROIs from 
+preprocessed 4D timeseries images.
+This script requires installation of biswebnode (npm install biswebnode). Other
+files/packages may be required - see below links for further details:
+https://www.npmjs.com/package/biswebnode
+https://bioimagesuiteweb.github.io/bisweb-manual/CommandLineTools.html
+Only include the 4d timeseries .nii images in your output directory.
+
+Note: these lines of code were ran separately in the Windows Powershell, the full script was
+note executed.
+
+Author: Rory Boyle rorytboyle@gmail.com
+Date: 13/05/2020
+#>
+
+# Enter your output directory name here
+Set-Location "A:\ROI_timeseries"
+
+# Read in files from input directory
+$files = Get-ChildItem -Path 'A:\preprocessed_4D_timeseries' -Recurse -Include *.nii
+
+# Loop through files, get subid, call biswebnode and compute ROIs, save output as subid_timeseries.csv
+foreach($file in $files){$subid = $file.Name.Substring(0,7); biswebnode computeroi -i $file.fullname -r A:\shen_2mm_268_parcellation.nii -o $subid’_timeseries.csv’}
